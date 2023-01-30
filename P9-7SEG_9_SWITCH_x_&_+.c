8ef8c61398c4dfcf622bb3bd60f853956c2943f2
MH11 <mamad.mh11@gmail.com>
2023-01-31 00:48:02 +0330

    Create P9-7SEG_9_SWITCH_x_&_+.c

---
+++ P9-7SEG_9_SWITCH_x_&_+.c
@@ -0,0 +1,25 @@
+#include <mega16.h>
+
+int seg[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+void main() {
+    DDRA = 0x00;
+    DDRB = 0x00;
+    DDRC = 0x00;
+    DDRD = 0xFF;
+    PORTA.0 = 1;
+    PORTA.1 = 1;
+    PORTB.0 = 1;
+    PORTB.1 = 1;
+    PORTC.0 = 1;
+    
+    while(1) {
+        if(PINC.0 == 0) {
+            PORTD = seg[PINA + PINB];
+        }
+        else
+        {
+            PORTD = seg[PINA * PINB];
+        }
+    }
+}
