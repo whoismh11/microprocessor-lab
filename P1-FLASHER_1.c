3d1431e57d8aab4b1f0cd4acc5dd04e7de18ba16
MH11 <mamad.mh11@gmail.com>
2023-01-31 00:41:24 +0330

    Create P1-FLASHER_1.c

---
+++ P1-FLASHER_1.c
@@ -0,0 +1,19 @@
+#include <mega16.h>
+#include <delay.h>
+
+int i;
+
+void main() {
+    DDRC = 0xFF;
+    
+    while(1) {
+        for(i = 1; i < 128; i = i*2) {
+            PORTC = i;
+            delay_ms(100);
+        }
+        for(i = 128; i > 1; i = i/2) {
+            PORTC = i;
+            delay_ms(100);
+        }
+    }
+}
