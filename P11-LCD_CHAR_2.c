e7c56623bf8726dc4dbfe77e2c5df72eded4f568
MH11 <mamad.mh11@gmail.com>
2023-01-31 00:50:30 +0330

    Create P11-LCD_CHAR_2.c

---
+++ P11-LCD_CHAR_2.c
@@ -0,0 +1,26 @@
+#include <mega16.h>
+#include <delay.h>
+#include <alcd.h>
+
+int i, j;
+char lcd1[] = "      Mamad";
+char lcd2[] = "      Hadad";
+
+void main() {
+    lcd_init(16);
+    
+    while(1) {
+        for(i = 6; i < 11; i++) {
+            lcd_gotoxy(i, 0);
+            lcd_putchar(lcd1[i]);
+            delay_ms(150);
+        }
+        for(j = 6; j < 11; j++) {
+            lcd_gotoxy(j, 1);
+            lcd_putchar(lcd2[j]);
+            delay_ms(150);
+        }
+        lcd_clear();
+        delay_ms(150);
+    }
+}
