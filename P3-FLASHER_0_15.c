cafa6554fa820bf4e17de636632756444c23ab98
MH11 <mamad.mh11@gmail.com>
2023-01-31 00:42:33 +0330

    Create P3-FLASHER_0_15.c

---
+++ P3-FLASHER_0_15.c
@@ -0,0 +1,26 @@
+#include <mega16.h>
+#include <delay.h>
+
+int i;
+
+void main() {
+    DDRA = 0xFF;
+    DDRC.0 = 0;
+    PORTC.0 = 1;
+    
+    while(1) {
+        if(PINC == 1) {
+            for(i = 0; i < 16; i++) {
+                PORTA = i;
+                delay_ms(1000);
+                if(PINC == 0) {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            PORTA = 0x00;
+        }
+    }
+}
