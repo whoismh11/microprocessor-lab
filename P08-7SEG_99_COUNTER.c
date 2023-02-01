d16dc972fb46bbf0382e3045c320df91dcf1eec8
MH11 <mamad.mh11@gmail.com>
2023-02-02 00:19:38 +0330

    Update P08-7SEG_99_COUNTER.c

--- P08-7SEG_99_COUNTER.c
+++ P08-7SEG_99_COUNTER.c
@@ -1,17 +1,16 @@
 #include <mega16.h>
 #include <delay.h>
 
-int i, j;
-int seg[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int i, j, seg[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 void main() {
     DDRC = 0xFF;
     DDRD = 0xFF;
-    
-    while(1) {
-        for(i = 0; i <= 9; i++) {
+
+    while (1) {
+        for (i = 0; i <= 9; i++) {
             PORTD = seg[i];
-            for(j = 0; j <= 9; j++) {
+            for (j = 0; j <= 9; j++) {
                 PORTC = seg[j];
                 delay_ms(300);
             }
