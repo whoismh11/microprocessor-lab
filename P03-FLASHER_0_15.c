5364b78c1181adcc6b20b27ce55e9d47caf5effd
MH11 <mamad.mh11@gmail.com>
2023-02-02 00:11:52 +0330

    Update P03-FLASHER_0_15.c

--- P03-FLASHER_0_15.c
+++ P03-FLASHER_0_15.c
@@ -7,19 +7,17 @@ void main() {
     DDRA = 0xFF;
     DDRC.0 = 0;
     PORTC.0 = 1;
-    
-    while(1) {
-        if(PINC == 1) {
-            for(i = 0; i < 16; i++) {
+
+    while (1) {
+        if (PINC == 1) {
+            for (i = 0; i < 16; i++) {
                 PORTA = i;
                 delay_ms(1000);
-                if(PINC == 0) {
+                if (PINC == 0) {
                     break;
                 }
             }
-        }
-        else
-        {
+        } else {
             PORTA = 0x00;
         }
     }
