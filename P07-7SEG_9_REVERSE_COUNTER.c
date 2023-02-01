ba25c74870c3e8ca9885db6c12c2fa44aa383e3a
MH11 <mamad.mh11@gmail.com>
2023-02-02 00:18:12 +0330

    Update P07-7SEG_9_REVERSE_COUNTER.c

--- P07-7SEG_9_REVERSE_COUNTER.c
+++ P07-7SEG_9_REVERSE_COUNTER.c
@@ -1,14 +1,13 @@
 #include <mega16.h>
 #include <delay.h>
 
-int i;
-int seg[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int i, seg[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 void main() {
     DDRC = 0xFF;
-    
-    while(1) {
-        for(i = 9; i >= 0; i--) {
+
+    while (1) {
+        for (i = 9; i >= 0; i--) {
             PORTC = seg[i];
             delay_ms(1000);
         }
