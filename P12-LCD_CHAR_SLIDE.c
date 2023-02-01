becccfebbb4a90f0e85626021e2979db691a776e
MH11 <mamad.mh11@gmail.com>
2023-02-02 00:24:03 +0330

    Update P12-LCD_CHAR_SLIDE.c

--- P12-LCD_CHAR_SLIDE.c
+++ P12-LCD_CHAR_SLIDE.c
@@ -7,26 +7,24 @@ char lcd[] = "MH11";
 
 void main() {
     lcd_init(16);
-    
-    while(1) {
-        for(i = 16; i > -5; i--) {
-            if(i < 0 & i > -5) {
+
+    while (1) {
+        for (i = 16; i > -5; i--) {
+            if (i < 0 & i > -5) {
                 lcd_gotoxy(0, 0);
-            }
-            else
-            {
+            } else {
                 lcd_gotoxy(i, 0);
             }
-            if(i < 16 & i > -1) {
+            if (i < 16 & i > -1) {
                 lcd_putchar(lcd[0]);
             }
-            if(i < 15 & i > -2) {
+            if (i < 15 & i > -2) {
                 lcd_putchar(lcd[1]);
             }
-            if(i < 14 & i > -3) {
+            if (i < 14 & i > -3) {
                 lcd_putchar(lcd[2]);
             }
-            if(i < 13 & i > -4) {
+            if (i < 13 & i > -4) {
                 lcd_putchar(lcd[3]);
             }
             delay_ms(100);
