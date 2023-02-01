3b1a325c3188cb57ba4c5b4b645d3976f2d30669
MH11 <mamad.mh11@gmail.com>
2023-02-02 00:14:00 +0330

    Update P04-SWITCH_LED.c

--- P04-SWITCH_LED.c
+++ P04-SWITCH_LED.c
@@ -6,13 +6,11 @@ void main() {
     DDRA.4 = 0;
     PORTA.0 = 1;
     PORTA.4 = 1;
-    
-    while(1) {
-        if(PINA.0 == 0 & PINA.4 == 0) {
+
+    while (1) {
+        if (PINA.0 == 0 & PINA.4 == 0) {
             PORTC.3 = 1;
-        }
-        else
-        {
+        } else {
             PORTC.3 = 0;
         }
     }
