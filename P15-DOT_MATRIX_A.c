449896593091920e6472fe5d7ec046c16130c425
MH11 <mamad.mh11@gmail.com>
2023-02-02 00:26:40 +0330

    Update P15-DOT_MATRIX_A.c

--- P15-DOT_MATRIX_A.c
+++ P15-DOT_MATRIX_A.c
@@ -6,11 +6,11 @@ int i, arr[8] = {0b00011000, 0b00111100, 0b01100110, 0b01100110, 0b01111110, 0b0
 void main() {
     DDRA = 0xFF;
     DDRB = 0xFF;
-    
-    while(1) {
-        for(i = 0; i < 8; i++) {
+
+    while (1) {
+        for (i = 0; i < 8; i++) {
             PORTA = arr[i];
-            PORTB = ~(1<<i);
+            PORTB = ~(1 << i);
             delay_us(10);
             PORTB = 0xFF;
         }
