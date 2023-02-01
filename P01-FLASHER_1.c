c4be126709eca947e05c93b482d233ff91df28f2
MH11 <mamad.mh11@gmail.com>
2023-02-02 00:09:28 +0330

    Update P01-FLASHER_1.c

--- P01-FLASHER_1.c
+++ P01-FLASHER_1.c
@@ -5,13 +5,13 @@ int i;
 
 void main() {
     DDRC = 0xFF;
-    
-    while(1) {
-        for(i = 1; i < 128; i = i*2) {
+
+    while (1) {
+        for (i = 1; i < 128; i = i * 2) {
             PORTC = i;
             delay_ms(100);
         }
-        for(i = 128; i > 1; i = i/2) {
+        for (i = 128; i > 1; i = i / 2) {
             PORTC = i;
             delay_ms(100);
         }
