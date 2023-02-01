3f7d38f4cdf02c4b38f24fbe79852c13002709a3
MH11 <mamad.mh11@gmail.com>
2023-02-02 00:25:23 +0330

    Update P14-LCD_DATE_CLOCK.c

--- P14-LCD_DATE_CLOCK.c
+++ P14-LCD_DATE_CLOCK.c
@@ -8,14 +8,14 @@ char lcd[16];
 
 void main() {
     lcd_init(16);
-    
-    while(1) {
-        for(y = 0; y < 10000; y++) {
-            for(x = 0; x < 13; x++) {
-                for(d = 0; d < 32; d++) {
-                    for(h = 0; h < 24; h++) {
-                        for(m = 0; m < 60; m++) {
-                            for(s = 0; s < 60; s++) {
+
+    while (1) {
+        for (y = 0; y < 10000; y++) {
+            for (x = 0; x < 13; x++) {
+                for (d = 0; d < 32; d++) {
+                    for (h = 0; h < 24; h++) {
+                        for (m = 0; m < 60; m++) {
+                            for (s = 0; s < 60; s++) {
                                 lcd_gotoxy(4, 0);
                                 sprintf(lcd, "%02d:%02d:%02d", h, m, s);
                                 lcd_puts(lcd);
