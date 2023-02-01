7a2c24f7369bc4b20172ad44e169f5ef63f74dc5
MH11 <mamad.mh11@gmail.com>
2023-02-02 00:15:12 +0330

    Update P05-FULL_ADDER.c

--- P05-FULL_ADDER.c
+++ P05-FULL_ADDER.c
@@ -4,8 +4,8 @@ void main() {
     DDRC = 0xFF;
     DDRA = 0x00;
     PORTA = 0xFF;
-    
-    while(1) {
+
+    while (1) {
         PORTC.0 = PINA.0 ^ PINA.3 ^ PINA.7;
         PORTC.7 = (PINA.0 & PINA.3) | (PINA.7 & (PINA.0 ^ PINA.3));
     }
